d3c524c85616c5416f04b31a7fe39ba0604f40a9
Frantz680 <dubdub68250@hotmail.fr>
2020-09-25 16:10:49 +0200

    Ajout de la convention PEP8

--- playability.py
+++ playability.py
@@ -1,134 +1,137 @@
+"""
+The class allows the player to move
+"""
+
 from init_pygame import *
 from constantes import *
 
-"""This class serves to move the hero but also
-to place the objects."""
-
 
 class MoveHero():
+    """
+    This class serves to move the hero but also
+    to place the objects.
+    """
 
-    """We build the constructor."""
     def __init__(self):
-        self.find_ether = False
-        self.find_syringe = False
-        self.find_needle = False
-        self.lost_game = False
-        self.win_game = False
-
-    """Resetting variables for when the game starts and restart."""
-    def init(self):
-        self.find_syringe = False
-        self.find_ether = False
-        self.find_needle = False
+        """
+        We build the constructor.
+        """
+        self.find_s = False
+        self.find_e = False
+        self.find_n = False
         self.win_game = False
         self.lost_game = False
-        self.position_perso_map_V = 0
-        self.position_perso_map_H = 0
+        self.map_hero_V = 0
+        self.map_hero_H = 0
 
-    """We move the person."""
-    def move(self,param_event,param_map):
+    def move(self, p_event, p_map):
+        """
+        We move the person.
+        """
 
-        if param_event == K_DOWN:  # If "down arrow"
-            if self.position_perso_map_V != 14:
-                if param_map.map_game[self.position_perso_map_V + 1][self.position_perso_map_H] != 'W':
+        if p_event == K_DOWN:  # If "down arrow"
+            if self.map_hero_V != 14:
+                if p_map.map_game[self.map_hero_V+1][self.map_hero_H] != 'W':
 
                     # We go down the hero GRAPHICALLY
-                    param_map.position_pixel_perso = param_map.position_pixel_perso.move(0, CASE_HEIGHT)
+                    p_map.pixel_hero = p_map.pixel_hero.move(0, CASE_H)
 
                     # We go down the hero PHYSICALLY
-                    self.position_perso_map_V = self.position_perso_map_V + 1
+                    self.map_hero_V = self.map_hero_V + 1
 
-                    print("position in map horizontally : " + str(self.position_perso_map_H))
-                    print("position in map vertically : " + str(self.position_perso_map_V))
-                    print(param_map.map_game[self.position_perso_map_V][self.position_perso_map_H])
+                    print("position in map H : " + str(self.map_hero_H))
+                    print("position in map V : " + str(self.map_hero_V))
+                    print(p_map.map_game[self.map_hero_V][self.map_hero_H])
 
                 else:
                     print("wall move down impossible")
-                    print("position in map horizontally : " + str(self.position_perso_map_H))
-                    print("position in map vertically : " + str(self.position_perso_map_V))
+                    print("position in map H : " + str(self.map_hero_H))
+                    print("position in map V : " + str(self.map_hero_V))
 
-        elif param_event == K_UP:  # If "up arrow"
-            if self.position_perso_map_V != 0:
-                if param_map.map_game[self.position_perso_map_V - 1][self.position_perso_map_H] != 'W':
+        elif p_event == K_UP:  # If "up arrow"
+            if self.map_hero_V != 0:
+                if p_map.map_game[self.map_hero_V-1][self.map_hero_H] != 'W':
 
                     # We mount the hero GRAPHICALLY
-                    param_map.position_pixel_perso = param_map.position_pixel_perso.move(0, -CASE_HEIGHT)
+                    p_map.pixel_hero = p_map.pixel_hero.move(0, -CASE_H)
 
                     # We mount the hero PHYSICALLY
-                    self.position_perso_map_V = self.position_perso_map_V - 1
+                    self.map_hero_V = self.map_hero_V - 1
 
-                    print("position in map horizontally : " + str(self.position_perso_map_H))
-                    print("position in map vertically : " + str(self.position_perso_map_V))
-                    print(param_map.map_game[self.position_perso_map_V][self.position_perso_map_H])
+                    print("position in map H : " + str(self.map_hero_H))
+                    print("position in map V : " + str(self.map_hero_V))
+                    print(p_map.map_game[self.map_hero_V][self.map_hero_H])
 
                 else:
                     print("wall moving up impossible")
-                    print("position in map horizontally : " + str(self.position_perso_map_H))
-                    print("position in map vertically : " + str(self.position_perso_map_V))
+                    print("position in map H : " + str(self.map_hero_H))
+                    print("position in map V : " + str(self.map_hero_V))
 
-        elif param_event == K_LEFT:  # If "left arrow"
-            if self.position_perso_map_H != 0:
-                if param_map.map_game[self.position_perso_map_V][self.position_perso_map_H - 1] != 'W':
+        elif p_event == K_LEFT:  # If "left arrow"
+            if self.map_hero_H != 0:
+                if p_map.map_game[self.map_hero_V][self.map_hero_H-1] != 'W':
 
                     # The hero goes left GRAPHICALLY
-                    param_map.position_pixel_perso = param_map.position_pixel_perso.move(-CASE_LENGTH, 0)
+                    p_map.pixel_hero = p_map.pixel_hero.move(-CASE_L, 0)
 
                     # The hero goes to the left PHYSICALLY
-                    self.position_perso_map_H = self.position_perso_map_H - 1
+                    self.map_hero_H = self.map_hero_H - 1
 
-                    print("position in map horizontally : " + str(self.position_perso_map_H))
-                    print("position in map vertically : " + str(self.position_perso_map_V))
-                    print(param_map.map_game[self.position_perso_map_V][self.position_perso_map_H])
+                    print("position in map H : " + str(self.map_hero_H))
+                    print("position in map V : " + str(self.map_hero_V))
+                    print(p_map.map_game[self.map_hero_V][self.map_hero_H])
 
                 else:
                     print("wall move to the left impossible")
-                    print("position in map horizontally : " + str(self.position_perso_map_H))
-                    print("position in map vertically : " + str(self.position_perso_map_V))
+                    print("position in map H : " + str(self.map_hero_H))
+                    print("position in map V : " + str(self.map_hero_V))
 
-        elif param_event == K_RIGHT:  # If "right arrow"
-            if self.position_perso_map_H != 14:
-                if param_map.map_game[self.position_perso_map_V][self.position_perso_map_H + 1] != 'W':
+        elif p_event == K_RIGHT:  # If "right arrow"
+            if self.map_hero_H != 14:
+                if p_map.map_game[self.map_hero_V][self.map_hero_H+1] != 'W':
 
                     # The hero goes right GRAPHICALLY
-                    param_map.position_pixel_perso = param_map.position_pixel_perso.move(CASE_LENGTH, 0)
+                    p_map.pixel_hero = p_map.pixel_hero.move(CASE_L, 0)
 
                     # The hero goes right PHYSICALLY
-                    self.position_perso_map_H = self.position_perso_map_H + 1
+                    self.map_hero_H = self.map_hero_H + 1
 
-                    print("position in map horizontally : " + str(self.position_perso_map_H))
-                    print("position in map vertically : " + str(self.position_perso_map_V))
-                    print(param_map.map_game[self.position_perso_map_V][self.position_perso_map_H])
+                    print("position in map H : " + str(self.map_hero_H))
+                    print("position in map V : " + str(self.map_hero_V))
+                    print(p_map.map_game[self.map_hero_V][self.map_hero_H])
 
                 else:
                     print("wall movement to the right not possible")
-                    print("position in map horizontally : " + str(self.position_perso_map_H))
-                    print("position in map vertically : " + str(self.position_perso_map_V))
+                    print("position in map H : " + str(self.map_hero_H))
+                    print("position in map V : " + str(self.map_hero_V))
 
-    """Randomly placing objects"""
-    def management_found_objects(self, param_map):
+    def management_found_objects(self, p_map):
+        """
+        Randomly placing objects.
+        """
 
         # If the hero goes on the object, the value becomes true
-        if param_map.map_game[self.position_perso_map_V][self.position_perso_map_H] == 'E':
+        if p_map.map_game[self.map_hero_V][self.map_hero_H] == 'E':
             print("You found the ether")
-            self.find_ether = True
+            self.find_e = True
 
-        elif param_map.map_game[self.position_perso_map_V][self.position_perso_map_H] == 'T':
+        elif p_map.map_game[self.map_hero_V][self.map_hero_H] == 'T':
             print("You have found the syringe")
-            self.find_syringe = True
+            self.find_s = True
 
-        elif param_map.map_game[self.position_perso_map_V][self.position_perso_map_H] == 'A':
+        elif p_map.map_game[self.map_hero_V][self.map_hero_H] == 'A':
             print("You found the needle")
-            self.find_needle = True
+            self.find_n = True
 
         # If the hero is on the guardian
-        elif param_map.map_game[self.position_perso_map_V][self.position_perso_map_H] == 'G':
+        elif p_map.map_game[self.map_hero_V][self.map_hero_H] == 'G':
 
             # If he finds all the objects he wins
-            if self.find_ether == True and self.find_needle == True and self.find_syringe == True:
+            if self.find_e and self.find_n and self.find_s:
                 self.win_game = True
                 print("You have won")
 
             # If he doesn't find all the objects he dies
-            elif self.find_ether == False or self.find_needle == False or self.find_syringe == False:
+            elif not self.find_e or not self.find_n or not self.find_s:
                 self.lost_game = True
                 print("You are dead")
